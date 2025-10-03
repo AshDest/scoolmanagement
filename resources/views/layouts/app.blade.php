987740ccb0fd6fe8eb95a37e3ce1f3ff54ac2cc4
AshDest <ashuzamaheshe4@gmail.com>
2025-10-03 21:19:08 +0200

    Remove vite css in app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'SchoolManager') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite(['resources/sass/app.scss','resources/js/app.js'])
     @livewireStyles
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <style>
